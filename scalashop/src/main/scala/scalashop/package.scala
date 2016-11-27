2d928d52a9dcc440c6acdc2eb453180dadaa50e3
julian <julianroche@gmail.com>
2016-11-27 17:16:12 +0000

    Removed println

--- scalashop/src/main/scala/scalashop/package.scala
+++ scalashop/src/main/scala/scalashop/package.scala
@@ -34,15 +34,11 @@ package object scalashop {
   class Img(val width: Int, val height: Int, private val data: Array[RGBA]) {
     def this(w: Int, h: Int) = this(w, h, new Array(w * h))
     def apply(x: Int, y: Int): RGBA = data(y * width + x)
-    def update(x: Int, y: Int, c: RGBA): Unit = {
-      println("updating c = " + c)
-      data(y * width + x) = c
-    }
+    def update(x: Int, y: Int, c: RGBA): Unit = data(y * width + x) = c
   }
 
   /** Computes the blurred RGBA value of a single pixel of the input image.    */
   def boxBlurKernel(src: Img, x: Int, y: Int, radius: Int): RGBA = {
-    //println("x = " + x + "; y = " + y  +"; r = " + radius)
     var xi = clamp(x - radius, 0, src.width)
     var yi = clamp(y - radius, 0, src.height)
     var mr = 0
