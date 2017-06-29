45aa301ef8ececb8ab8ddae18c0b99d0b650dffe
fjro <julianroche@gmail.com>
2017-06-29 19:25:04 +0100

    Initial file based implementation

--- observatory/src/main/scala/observatory/Extraction.scala
+++ observatory/src/main/scala/observatory/Extraction.scala
@@ -1,11 +1,87 @@
 package observatory
 
 import java.time.LocalDate
+import java.io
+import scala.collection.mutable.Map
+import scala.io.{BufferedSource, Source}
 
 /**
   * 1st milestone: data extraction
   */
 object Extraction {
+
+  type stationID = (Int, Int)
+  type station = (stationID, Location)
+  type observation = (LocalDate, Double)
+  type stationObservation = (stationID, observation)
+
+  def parseStation(s: String): Option[station] = {
+    try {
+      val parts = s.split(",")
+      if (parts.length == 4) {
+        return Some(((parts(0).toInt, parts(1).toInt), Location(parts(2).toDouble, parts(3).toDouble)))
+      }
+      else
+        return None
+    }
+    catch {
+      case e: Exception => None
+    }
+  }
+
+  def readStations(fileName: String): Map[stationID, Location] = {
+    val stations = Map[stationID, Location]()
+    val src: BufferedSource = Source.fromInputStream(getClass.getResourceAsStream(fileName))
+
+    try {
+      val it: Iterator[String] = src.getLines()
+      while(it.hasNext) {
+        val line = it.next()
+        val station = parseStation(line)
+        if (station.isDefined)
+          stations +=  station.get._1 -> station.get._2
+      }
+
+      return stations
+    }
+    finally
+      src.close
+  }
+
+  def parseTemp(s: String, year: Int): Option[stationObservation] = {
+    try {
+      val parts = s.split(",")
+      if (parts.length == 5) {
+        val si = (parts(0).toInt, parts(1).toInt)
+        val obs = (LocalDate.of(year, parts(2).toInt, parts(3).toInt), parts(4).toDouble )
+        return Some( (si, obs) )
+      }
+      else
+        return None
+    }
+    catch {
+      case e: Exception => None
+    }
+  }
+
+  def readTemps(year: Int, fileName: String): Map[stationID, observation] = {
+    val temps = Map[stationID, observation]()
+    val src: BufferedSource = Source.fromInputStream(getClass.getResourceAsStream(fileName))
+
+    try {
+      val it: Iterator[String] = src.getLines
+      while(it.hasNext) {
+        val line = it.next()
+        val temp = parseTemp(line, year)
+        if (temp.isDefined)
+          temps +=  temp.get._1 -> temp.get._2
+      }
+
+      return temps
+    }
+    finally
+      src.close
+  }
 
   def fahrenheitToCelsius(f: Double): Double =
     (f - 32.0) * (5.0/9.0)
@@ -17,7 +93,9 @@ object Extraction {
     * @return A sequence containing triplets (date, location, temperature)
     */
   def locateTemperatures(year: Int, stationsFile: String, temperaturesFile: String): Iterable[(LocalDate, Location, Double)] = {
-    ???
+    val stations = readStations(stationsFile)
+    val temps = readTemps(year, temperaturesFile)
+    return stations.filter(s => temps.contains(s._1)).map(s => (temps(s._1)._1,  s._2, temps(s._1)._2))
   }
 
   /**
--- observatory/src/test/scala/observatory/ExtractionTest.scala
+++ observatory/src/test/scala/observatory/ExtractionTest.scala
@@ -11,5 +11,30 @@ class ExtractionTest extends FunSuite {
   test("fahrenheit conversion") {
     assert(fahrenheitToCelsius(100.0) === 37.77777777777778, "Temp conversion failed")
   }
+
+  test("parseStation") {
+    assert(parseStation("") === None)
+    assert(parseStation(null) === None)
+    assert(parseStation("11,") === None)
+    assert(parseStation("1,2,+11.111,+222.222").get._2 === Location(11.111,222.222))
+  }
+
+  test("readStations") {
+    val stations = readStations("/stations.csv")
+    assert(stations.size === 2818)
+  }
+
+  test("readTemps") {
+    val temps1975 = readTemps(1975, "/1975.csv")
+    assert(temps1975.size === 721)
+
+    val temps1976 = readTemps(1976, "/1976.csv")
+    assert(temps1976.size === 722)
+  }
+
+  test("locateTemperatures") {
+    val temps = locateTemperatures(1975, "/stations.csv", "/1975.csv").toList
+    assert(temps.length === 720)
+  }
   
 }
